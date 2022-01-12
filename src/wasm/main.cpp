df856c677dab728939d9c69bd7f906bab0ca2df5
ericcolvinmorgan <accounts@ericcolvinmorgan.com>
2022-01-11 22:29:50 -0800

    Basic Emscripten SDL2 web interface

--- src/wasm/main.cpp
+++ src/wasm/main.cpp
@@ -2,20 +2,58 @@
 // AND https://emscripten.org/docs/getting_started/Tutorial.html
 
 #include <stdio.h>
+#include <SDL2/SDL.h>
 #include <emscripten/emscripten.h>
+#include "../../include/emulator/emulator.h"
 
-int main() {
-    printf("Hello World\n");
+const int SCREEN_WIDTH = 256;
+const int SCREEN_HEIGHT = 224;
+
+Uint8 r = 0x00;
+Uint8 g = 0x00;
+Uint8 b = 0xFF;
+
+SDL_Window *window = nullptr;
+Emulator *emulator =nullptr;
+
+void renderContent()
+{
+    emulator->advanceFrame();
+    SDL_Surface *screenSurface = SDL_GetWindowSurface(window);
+    Uint8 temp = b;
+    b = g;
+    g = r;
+    r = temp;
+    SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, r, g, b));
+    SDL_UpdateWindowSurface(window);
+    SDL_Delay(1000);
+    printf("Current Frame: %d\n", emulator->getFrame());
 }
 
-#ifdef __cplusplus
-extern "C" {
-#endif
+int main()
+{
+    // Initialize
+    emulator = new Emulator();
+    if (SDL_Init(SDL_INIT_VIDEO) < 0)
+    {
+        printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+    }
+    else
+    {
+        window = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+        if (window == NULL)
+        {
+            printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+        }
+        else
+        {
+            emscripten_set_main_loop(renderContent, 0, 1);
+        }
+    }
 
-EMSCRIPTEN_KEEPALIVE void myFunction(int argc, char ** argv) {
-    printf("MyFunction Called\n");
-}
+    // Clean-up
+    SDL_DestroyWindow(window);
+    SDL_Quit();
 
-#ifdef __cplusplus
+    return 0;
 }
-#endif
