ace5eff078140c362600b967325f7b44c0a4b128
ericcolvinmorgan <accounts@ericcolvinmorgan.com>
2022-01-13 23:25:26 -0800

    Added status register struct

--- include/emulator/cpu.h
+++ include/emulator/cpu.h
@@ -2,15 +2,36 @@
 
 #include "constants.h"
 
+struct Flags
+{
+    BYTE n : 1; // Negative Flag
+    BYTE o : 1; // Overflow Flag
+    BYTE : 1;   // Unused - Documentation indicates this seems to always be set to 1.
+    BYTE b : 1; // Break Flag
+    BYTE d : 1; // Decimal Mode Flag
+    BYTE i : 1; // Interrupt Disable Flag
+    BYTE z : 1; // Zero Flag
+    BYTE c : 1; // Carry Flag
+};
+
+struct StatusRegister
+{
+    union
+    {
+        struct Flags flags;
+        BYTE data;
+    };
+};
+
 class CPU
 {
 private:
-    BYTE _a = 0;  // Accumulator
-    BYTE _x = 0;  // X Index
-    BYTE _y = 0;  // Y Index
-    BYTE _sp = 0; // Stack Pointer
-    BYTE _sr = 0; // Status Register
-    WORD _pc = 0; // Program Counter
+    BYTE _a = 0;                                     // Accumulator
+    BYTE _x = 0;                                     // X Index
+    BYTE _y = 0;                                     // Y Index
+    BYTE _sp = 0;                                    // Stack Pointer
+    struct StatusRegister _sr = {data : 0b00000100}; // Status Register
+    WORD _pc = 0;                                    // Program Counter
 
 public:
     CPU(){};
