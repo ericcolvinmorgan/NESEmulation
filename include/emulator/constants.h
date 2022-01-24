47dc69c67cc3acde4b913c3303edca6ff0ce3cdd
ericcolvinmorgan <accounts@ericcolvinmorgan.com>
2022-01-23 22:40:30 -0800

    Fixed flag consts to match union order.

--- include/emulator/constants.h
+++ include/emulator/constants.h
@@ -6,10 +6,10 @@ typedef uint8_t Byte;
 typedef uint16_t Word;
 
 const Word kReset = 0xFFFC;
-const Byte kCarryFlag       = 1;
-const Byte kZeroFlag        = 1 << 1;
-const Byte kInterruptFlag   = 1 << 2;
-const Byte kDecimalFlag     = 1 << 3;
-const Byte kBreakFlag       = 1 << 4;
-const Byte kOverflowFlag    = 1 << 6;
-const Byte kNegativeFlag    = 1 << 7;
+const Byte kCarryFlag = 1 << 7;
+const Byte kZeroFlag = 1 << 6;
+const Byte kInterruptFlag = 1 << 5;
+const Byte kDecimalFlag = 1 << 4;
+const Byte kBreakFlag = 1 << 3;
+const Byte kOverflowFlag = 1 << 1;
+const Byte kNegativeFlag = 1;
