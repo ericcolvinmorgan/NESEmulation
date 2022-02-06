52041469e7cc2c1c7c85b1ecfba454e4f18483ae
ericcolvinmorgan <accounts@ericcolvinmorgan.com>
2022-02-06 15:53:58 -0800

    Updates to 6502 demo

--- include/emulator/emulator.h
+++ include/emulator/emulator.h
@@ -10,6 +10,7 @@ private:
     CPU *cpu_;
     OpCodesInterface *cpu_opcodes_;
     int frame_ = 0;
+    bool enable_logging_;
 
 public:
     Emulator(CPU *cpu, OpCodesInterface *opcodes, int cpu_cycles_per_frame = 29780)
@@ -21,4 +22,5 @@ public:
     ~Emulator(){};
     int GetFrame() { return frame_; };
     void AdvanceFrame();
+    void EnableLogging(bool enable_logging) { enable_logging_ = enable_logging; };
 };
--- src/emulator/emulator.cpp
+++ src/emulator/emulator.cpp
@@ -3,25 +3,29 @@
 
 void Emulator::AdvanceFrame()
 {
-    //Advance CPU
-    while(cpu_->GetCycleCount() < cpu_cycles_per_frame_)
+    // Advance CPU
+    while (cpu_->GetCycleCount() < cpu_cycles_per_frame_)
     {
-            auto opcode = cpu_->GetCurrentOpCode();
-            // printf("A=$%x X=$%x Y=$%x SP=$%x PC=$%x NV-BDIZC=%d%d%d%d%d%d%d%d OpCode=$%x\n", 
-            // cpu_->GetAccumulator(), cpu_->GetXIndex(), cpu_->GetYIndex(), cpu_->GetStackPointer(),
-            // cpu_->GetProgramCounter(), 
-            // cpu_->GetStatusRegister().flags.n,
-            // cpu_->GetStatusRegister().flags.o,
-            // 1,
-            // cpu_->GetStatusRegister().flags.b,
-            // cpu_->GetStatusRegister().flags.d,
-            // cpu_->GetStatusRegister().flags.i,
-            // cpu_->GetStatusRegister().flags.z,
-            // cpu_->GetStatusRegister().flags.c,
-            // opcode);
+        auto opcode = cpu_->GetCurrentOpCode();
 
-            cpu_->AdvanceProgramCounter();
-            cpu_opcodes_->RunOpCode(cpu_, opcode);
+        if (enable_logging_)
+        {
+            printf("A=$%x X=$%x Y=$%x SP=$%x PC=$%x NV-BDIZC=%d%d%d%d%d%d%d%d OpCode=$%x\n",
+                   cpu_->GetAccumulator(), cpu_->GetXIndex(), cpu_->GetYIndex(), cpu_->GetStackPointer(),
+                   cpu_->GetProgramCounter(),
+                   cpu_->GetStatusRegister().flags.n,
+                   cpu_->GetStatusRegister().flags.o,
+                   1,
+                   cpu_->GetStatusRegister().flags.b,
+                   cpu_->GetStatusRegister().flags.d,
+                   cpu_->GetStatusRegister().flags.i,
+                   cpu_->GetStatusRegister().flags.z,
+                   cpu_->GetStatusRegister().flags.c,
+                   opcode);
+        }
+
+        cpu_->AdvanceProgramCounter();
+        cpu_opcodes_->RunOpCode(cpu_, opcode);
     }
     cpu_->ResetCycleCount();
 }
