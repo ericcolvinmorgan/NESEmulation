763c6fa21348410a316e426796a95d5d8302adb1
adorgan <adorgan@gmail.com>
2022-02-17 12:32:28 -0600

    re-figured sprites evaluation, sprites showing in right place but coloring is off

--- src/emulator/ppu.cpp
+++ src/emulator/ppu.cpp
@@ -96,6 +96,20 @@ void PPU::HandleOAMDMAWrite(void *address)
     Byte data = cpu_memory_->ReadByte(*(uint16_t *)address);
     dma_addr_page = data;
     is_dma_transferring = true;  
+    if (is_dma_transferring){
+        for(int i = 0; i < 256; i++){
+            dma_data = cpu_memory_->ReadByte((dma_addr_page << 8) | oam_address_);
+            oam_[oam_address_] = dma_data;
+            oam_address_++;
+        }  
+        is_dma_transferring = false;
+        oam_address_ = 0;
+    } 
+
+    // for(int i = 0; i < 20; i++){
+    //     printf("OAM %d: (%d %d) ID: %x AT: %x\n", i, oam_[i*4 + 3], oam_[i*4 + 0], oam_[i*4 + 1], oam_[i*4 + 2]);
+    // }
+    
 }
 
 // still need to implement as handler
@@ -115,132 +129,122 @@ Byte PPU::HandlePPUDATARead()
 
 void PPU::RunEvents()
 {
-    switch (cycle_scanline_)
-    {
-    case 0 ... 239: // Visible Frame Lines
-    {
+    switch (cycle_scanline_) {
+        case 0 ... 239: // Visible Frame Lines
+        {
+            switch (cycle_pixel_) {
+                case 1 ... 256: {}
 
-        switch (cycle_pixel_)
-        {
-        case 1 ... 64: 
-        {
-            std::fill(secondary_oam_, secondary_oam_ + 32, (Byte)0xFF); // clear sprites
-        }
-        case 65 ... 256:
-        {
-            // sprite evaluation
-            int n = 0;
-            num_sprites = 0;
-            while (n < 64 && num_sprites < 9)
-            {
-                Byte y_pos = oam_[n * 4];
-                if (cycle_scanline_ - y_pos >= 0) // top position in range
-                {
-                    if (num_sprites < 8)
-                    {
-                        for (int i = 0; i < 4; i++)
+                case 257 ... 339: {
+
+                    // determine sprites for next scanline at end of current scanline
+                    if(cycle_scanline_ >= 0){
+                        std::fill(secondary_oam_, secondary_oam_ + 32, (Byte)0xFF); // clear sprites
+                        int n = 0;
+                        num_sprites = 0;
+                        while (n < 64 && num_sprites < 8) // 8 sprites max
                         {
-                            secondary_oam_[(num_sprites * 4) + i] = oam_[(n * 4) + i];
-                        }
-                        num_sprites++;
+                            Byte y_pos = oam_[n * 4];
+                            
+                            if ((cycle_scanline_ - y_pos) >= 0 && (cycle_scanline_ - y_pos) < 8) // sprite visible on next scanline
+                            {
+                                if (num_sprites < 8) // add sprites to secondary OAM to be rendered later
+                                {
+                                    for (int i = 0; i < 4; i++)
+                                    {
+                                        secondary_oam_[(num_sprites * 4) + i] = oam_[(n * 4) + i];
+                                    }
+                                    num_sprites++;
+                                }
+                            }
+                            n++;
+                        }          
                     }
+
+                    if ((cycle_pixel_ % 8) == 1) // Nametable Fetch
+                    {
+                    }
+                    else if ((cycle_pixel_ % 8) == 3) // Attribute table Fetch
+                    {
+                    }
+                    else if ((cycle_pixel_ % 8) == 5) // Low BG Tile Fetch
+                    {
+                    }
+                    else if ((cycle_pixel_ % 8) == 7) // High BG Tile Fetch
+                    {
+                    }
+                    break;
                 }
-                n++;
+                case 340:
+                {
+                    // render sprite at end of scanline
+                    RenderSprites();
+                    break;
+                }
+            }
+        }
+        break;
+
+        case 240 ... 260: // Post-Render Frame Lines
+        {
+            // Set VBlank
+            if (cycle_scanline_ == 241 && cycle_pixel_ == 1)
+            {
+                Byte status = cpu_memory_->ReadByte(kPPUSTATUS);
+                cpu_memory_->WriteMemory(kPPUSTATUS, (Byte)(status | 0b10000000), true);
+                if (reg_ctrl_.flags.nmi)
+                    nmi_requested_ = true;
             }
 
-            if (num_sprites == 9)
-            {
-                reg_status_.flags.overflow = 1;
-            }
             
         }
+        break;
 
-        case 321 ... 336:
+        case 261: // Pre-Render Frame Line
         {
-            if ((cycle_pixel_ % 8) == 1) // Nametable Fetch
+            // End VBlank
+            if (cycle_pixel_ == 1)
             {
+                Byte status = cpu_memory_->ReadByte(kPPUSTATUS);
+                cpu_memory_->WriteMemory(kPPUSTATUS, (Byte)(status | 0b00000000), true);
+
+                reg_status_.flags.overflow = 0;
             }
-            else if ((cycle_pixel_ % 8) == 3) // Attribute table Fetch
+
+            switch (cycle_pixel_)
             {
+            case 1 ... 256:
+            case 321 ... 336:
+            {
+                if ((cycle_pixel_ % 8) == 1) // Nametable Fetch
+                {
+                }
+                else if ((cycle_pixel_ % 8) == 3) // Attribute table Fetch
+                {
+                }
+                else if ((cycle_pixel_ % 8) == 5) // Low BG Tile Fetch
+                {
+                }
+                else if ((cycle_pixel_ % 8) == 7) // High BG Tile Fetch
+                {
+                }
             }
-            else if ((cycle_pixel_ % 8) == 5) // Low BG Tile Fetch
+            break;
+
+            // TO START WE WILL JUST LOAD EVERYTHING AT THE END OF THE PRE-RENDER LINE.  THIS IS NOT PROPER AND SHOULD BE REPLACED.
+            case 340:
             {
+                FillScreenBuffer();
             }
-            else if ((cycle_pixel_ % 8) == 7) // High BG Tile Fetch
-            {
+            break;
             }
         }
         break;
         }
-    }
-    break;
 
-    case 240 ... 260: // Post-Render Frame Lines
-    {
-        // Set VBlank
-        if (cycle_scanline_ == 241 && cycle_pixel_ == 1)
-        {
-            Byte status = cpu_memory_->ReadByte(kPPUSTATUS);
-            cpu_memory_->WriteMemory(kPPUSTATUS, (Byte)(status | 0b10000000), true);
-            if (reg_ctrl_.flags.nmi)
-                nmi_requested_ = true;
-        }
-
-        if (is_dma_transferring){
-            for(int i = 0; i < 256; i++){
-                dma_data = cpu_memory_->ReadByte((dma_addr_page << 8) | oam_address_);
-                oam_[oam_address_] = dma_data;
-                oam_address_++;
-            }
-            
-            is_dma_transferring = false;
-            oam_address_ = 0;
-        } 
-    }
-    break;
-
-    case 261: // Pre-Render Frame Line
-    {
-        // End VBlank
-        if (cycle_pixel_ == 1)
-        {
-            Byte status = cpu_memory_->ReadByte(kPPUSTATUS);
-            cpu_memory_->WriteMemory(kPPUSTATUS, (Byte)(status | 0b00000000), true);
-
-            reg_status_.flags.overflow = 0;
-        }
-
-        switch (cycle_pixel_)
-        {
-        case 1 ... 256:
-        case 321 ... 336:
-        {
-            if ((cycle_pixel_ % 8) == 1) // Nametable Fetch
-            {
-            }
-            else if ((cycle_pixel_ % 8) == 3) // Attribute table Fetch
-            {
-            }
-            else if ((cycle_pixel_ % 8) == 5) // Low BG Tile Fetch
-            {
-            }
-            else if ((cycle_pixel_ % 8) == 7) // High BG Tile Fetch
-            {
-            }
-        }
-        break;
-
-        // TO START WE WILL JUST LOAD EVERYTHING AT THE END OF THE PRE-RENDER LINE.  THIS IS NOT PROPER AND SHOULD BE REPLACED.
-        case 340:
-        {
-            FillScreenBuffer();
-            RenderSprites();
-        }
-        break;
-        }
-    }
-    break;
-    }
+    // for(int i = 0; i < num_sprites; i++){
+    //     printf("sprite %d: (%d, %d) ID: %x AT: %x\n", i, secondary_oam_[(num_sprites * i) + 3], secondary_oam_[(num_sprites * i)], secondary_oam_[(num_sprites * i) + 1], secondary_oam_[(num_sprites * i) + 2]);
+    // }
 }
 
 void PPU::IncrementPPUAddr()
@@ -325,31 +329,48 @@ void PPU::FillScreenBuffer()
 
 void PPU::RenderSprites()
 {
-
-    for (int i = 0; i < 8; i++)
+    for (int i = 0; i < num_sprites; i++)
     {
+        Word sprite_pt_addr_lo = 0x0; // stores sprite tile address
+
+        // test for vertical flip
+        Byte vert_flag = (secondary_oam_[i * num_sprites + 2] & 0b10000000) >> 7;
+        if(vert_flag) // flip vertically
+        {
+            Word base_pt_addr = reg_ctrl_.flags.sprite_addr ? 0x1000 : 0x0000;
+            Word id_offset = secondary_oam_[i * num_sprites + 1] * 16;
+            Byte y_offset = 7 - (cycle_scanline_ - secondary_oam_[i * num_sprites]);
+            sprite_pt_addr_lo = base_pt_addr | id_offset | y_offset;
+
+        }
+        else // normal orientation
+        {
+            Word base_pt_addr = reg_ctrl_.flags.sprite_addr ? 0x1000 : 0x0000;
+            Word id_offset = secondary_oam_[i * num_sprites + 1] * 16;
+            Byte y_offset = cycle_scanline_ - secondary_oam_[i * num_sprites];
+            sprite_pt_addr_lo = base_pt_addr | id_offset | y_offset;
+        }
         
+        // sprite bit planes
+        Byte sprite_tile_low_byte = ppu_memory_->ReadByte(sprite_pt_addr_lo);
+        Byte sprite_tile_high_byte = ppu_memory_->ReadByte(sprite_pt_addr_lo + 8);
+        
+        // position coordinate on screen for start of sprite
         Byte y_pos = secondary_oam_[i * 4];
-        Byte x_pos = secondary_oam_[(i * 4) + 3] - 15;
-        
-        Word patten_table_base = reg_ctrl_.flags.sprite_addr ? 0x1000 : 0x0000;
-        Byte tile_index = secondary_oam_[(i*4) + 1];
-        
-        Byte sprite_tile_low_byte = ppu_memory_->ReadByte(patten_table_base | tile_index);
-        Byte sprite_tile_high_byte = ppu_memory_->ReadByte((patten_table_base | tile_index) + 8);
+        Byte x_pos = secondary_oam_[(i * 4) + 3];
+
 
         for (int p_h = 0; p_h < 8; p_h++)
         {
             for (int p_w = 0; p_w < 8; p_w++)
             {
-                int base_index = ((y_pos * 30 * 8) + x_pos + (p_h * 8) + p_w);
-                uint8_t palette_index = ((sprite_tile_high_byte >> (7 - p_w)) & 0b00000001) | (((sprite_tile_low_byte >> (7 - p_w)) & 0b00000001) << 1);
+                int base_index = ((y_pos * 32 * 8) + (p_h * 256) + x_pos + p_w);
                 
-                //TODO fix color palette selection
-                if(base_index < 240 * 232){
-                    Byte color = ppu_memory_->ReadByte(0x3f11 + palette_index);
-                    screen_buffer_[base_index] = palette_index;
-                }
+                int base_palette = (secondary_oam_[i * 4 + 2] & 0b00000011); // indicates sprite palette 4-7
+                uint8_t palette_index = ((sprite_tile_high_byte >> (7 - p_w)) & 0b00000001) | (((sprite_tile_low_byte >> (7 - p_w)) & 0b00000001) << 1);
+
+                Byte color = ppu_memory_->ReadByte(0x3f11 + base_palette*4 + palette_index);
+                screen_buffer_[base_index] = color;
             }
         }
     }
