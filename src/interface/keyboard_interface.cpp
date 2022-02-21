c86e8a13c0dc1f402548df4665b1e4502afdea7b
ericcolvinmorgan <accounts@ericcolvinmorgan.com>
2022-02-21 13:20:04 -0800

    Fixes controller based on SDL2 documentation.

--- src/interface/keyboard_interface.cpp
+++ src/interface/keyboard_interface.cpp
@@ -3,31 +3,51 @@
 
 uint8_t KeyboardInterface::GetInputState() const
 {
+    SDL_Event event;
+    while (SDL_PollEvent(&event))
+    {
+        /* 
+            Poll all events. These will be handled by filters created elsewhere.
+            From https://wiki.libsdl.org/SDL_GetKeyboardState:
+            This function gives you the current state after all events have been processed, 
+            so if a key or button has been pressed and released before you process events, 
+            then the pressed state will never show up in the SDL_GetKeyboardState() calls.
+        */
+    }
+
     // SDL owns this state so we shouldn't free it
     const uint8_t *state = SDL_GetKeyboardState(NULL);
     uint8_t buttonStates = 0;
-    if (state[SDL_SCANCODE_J]) { // A
+    if (state[SDL_SCANCODE_J])
+    { // A
         buttonStates |= 1;
     }
-    if (state[SDL_SCANCODE_K]){ // B
+    if (state[SDL_SCANCODE_K])
+    { // B
         buttonStates |= (1 << 1);
     }
-    if (state[SDL_SCANCODE_SPACE]){ // Select
+    if (state[SDL_SCANCODE_SPACE])
+    { // Select
         buttonStates |= (1 << 2);
     }
-    if (state[SDL_SCANCODE_RETURN]){ // Start
+    if (state[SDL_SCANCODE_RETURN])
+    { // Start
         buttonStates |= (1 << 3);
     }
-    if (state[SDL_SCANCODE_UP] || state[SDL_SCANCODE_W]) { // Up
+    if (state[SDL_SCANCODE_UP] || state[SDL_SCANCODE_W])
+    { // Up
         buttonStates |= (1 << 4);
     }
-    if (state[SDL_SCANCODE_DOWN] || state[SDL_SCANCODE_S]){ // Down
+    if (state[SDL_SCANCODE_DOWN] || state[SDL_SCANCODE_S])
+    { // Down
         buttonStates |= (1 << 5);
     }
-    if (state[SDL_SCANCODE_LEFT] || state[SDL_SCANCODE_A]) { // Left
+    if (state[SDL_SCANCODE_LEFT] || state[SDL_SCANCODE_A])
+    { // Left
         buttonStates |= (1 << 6);
     }
-    if (state[SDL_SCANCODE_RIGHT] || state[SDL_SCANCODE_D]){ // Right
+    if (state[SDL_SCANCODE_RIGHT] || state[SDL_SCANCODE_D])
+    { // Right
         buttonStates |= (1 << 7);
     }
 
